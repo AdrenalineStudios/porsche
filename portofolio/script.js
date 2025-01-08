9bba5c35a3ee31390de8c9df4a331bf422d390db
Hadi Ali Ghaznawi <99736077+AdrenalineStudios@users.noreply.github.com>
2025-01-08 20:14:31 +0100

    Basic HTML site

---
+++ portofolio/script.js
@@ -0,0 +1,8 @@
+// ======================= Typing animation animation ==============================
+var typed = new Typed(".typing",{
+    strings:["","Web Designer", "Web Developer", "Game Developer", "Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
